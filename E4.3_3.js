1392c6e32ed48283d3b5e5b599a12a42540c832a
AnDRawer <gold93@rambler.ru>
2022-10-25 02:25:29 +0300

    First commit

---
+++ E4.3_3.js
@@ -0,0 +1,10 @@
+/*----------------------- Задание Е4.3_3--------------------------------
+Написать функцию, которая создает пустой объект, но без прототипа. */
+
+const createEmptyObject = function(){
+    const obj = Object.create(null);
+    return obj;
+  }
+
+  console.log('Создан пустой объект:')
+  console.log(createEmptyObject());
